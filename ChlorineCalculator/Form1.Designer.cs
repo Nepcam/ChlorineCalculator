72654fad9e095790255201cccca44a1ed5fdb805
Nepcam <cameron.nepe@gmail.com>
2020-03-19 16:34:45 +1300

    Add project files.

---
+++ ChlorineCalculator/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace ChlorineCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelVolumeOfWater = new System.Windows.Forms.Label();
+            this.labelChlorineNeeded = new System.Windows.Forms.Label();
+            this.labelAppTitle = new System.Windows.Forms.Label();
+            this.labelLength = new System.Windows.Forms.Label();
+            this.textBoxLengthOfPool = new System.Windows.Forms.TextBox();
+            this.labelWidthOfPool = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelDepthOfPool = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelKgsNeeded = new System.Windows.Forms.Label();
+            this.labelNumberOfBags = new System.Windows.Forms.Label();
+            this.labelCosts = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1VolumeTotal = new System.Windows.Forms.Label();
+            this.textBoxVolumeTotal = new System.Windows.Forms.TextBox();
+            this.buttonCalculateChlorine = new System.Windows.Forms.Button();
+            this.buttonClearAllFields = new System.Windows.Forms.Button();
+            this.buttonCloseApp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelVolumeOfWater
+            // 
+            this.labelVolumeOfWater.AutoSize = true;
+            this.labelVolumeOfWater.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVolumeOfWater.Location = new System.Drawing.Point(29, 54);
+            this.labelVolumeOfWater.Name = "labelVolumeOfWater";
+            this.labelVolumeOfWater.Size = new System.Drawing.Size(373, 25);
+            this.labelVolumeOfWater.TabIndex = 0;
+            this.labelVolumeOfWater.Text = "CALCULATE VOLUME OF WATER";
+            // 
+            // labelChlorineNeeded
+            // 
+            this.labelChlorineNeeded.AutoSize = true;
+            this.labelChlorineNeeded.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChlorineNeeded.Location = new System.Drawing.Point(465, 54);
+            this.labelChlorineNeeded.Name = "labelChlorineNeeded";
+            this.labelChlorineNeeded.Size = new System.Drawing.Size(227, 25);
+            this.labelChlorineNeeded.TabIndex = 1;
+            this.labelChlorineNeeded.Text = "CHLORINE NEEDED";
+            // 
+            // labelAppTitle
+            // 
+            this.labelAppTitle.AutoSize = true;
+            this.labelAppTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAppTitle.Location = new System.Drawing.Point(229, 9);
+            this.labelAppTitle.Name = "labelAppTitle";
+            this.labelAppTitle.Size = new System.Drawing.Size(366, 31);
+            this.labelAppTitle.TabIndex = 2;
+            this.labelAppTitle.Text = "CHLORINE CALCULATOR";
+            // 
+            // labelLength
+            // 
+            this.labelLength.AutoSize = true;
+            this.labelLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLength.Location = new System.Drawing.Point(31, 100);
+            this.labelLength.Name = "labelLength";
+            this.labelLength.Size = new System.Drawing.Size(161, 20);
+            this.labelLength.TabIndex = 3;
+            this.labelLength.Text = "LENGTH OF POOL";
+            // 
+            // textBoxLengthOfPool
+            // 
+            this.textBoxLengthOfPool.Location = new System.Drawing.Point(235, 100);
+            this.textBoxLengthOfPool.Name = "textBoxLengthOfPool";
+            this.textBoxLengthOfPool.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLengthOfPool.TabIndex = 4;
+            // 
+            // labelWidthOfPool
+            // 
+            this.labelWidthOfPool.AutoSize = true;
+            this.labelWidthOfPool.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWidthOfPool.Location = new System.Drawing.Point(32, 138);
+            this.labelWidthOfPool.Name = "labelWidthOfPool";
+            this.labelWidthOfPool.Size = new System.Drawing.Size(148, 20);
+            this.labelWidthOfPool.TabIndex = 5;
+            this.labelWidthOfPool.Text = "WIDTH OF POOL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(235, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // labelDepthOfPool
+            // 
+            this.labelDepthOfPool.AutoSize = true;
+            this.labelDepthOfPool.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDepthOfPool.Location = new System.Drawing.Point(33, 175);
+            this.labelDepthOfPool.Name = "labelDepthOfPool";
+            this.labelDepthOfPool.Size = new System.Drawing.Size(149, 20);
+            this.labelDepthOfPool.TabIndex = 7;
+            this.labelDepthOfPool.Text = "DEPTH OF POOL";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // labelKgsNeeded
+            // 
+            this.labelKgsNeeded.AutoSize = true;
+            this.labelKgsNeeded.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKgsNeeded.Location = new System.Drawing.Point(466, 100);
+            this.labelKgsNeeded.Name = "labelKgsNeeded";
+            this.labelKgsNeeded.Size = new System.Drawing.Size(43, 20);
+            this.labelKgsNeeded.TabIndex = 9;
+            this.labelKgsNeeded.Text = "KGs";
+            // 
+            // labelNumberOfBags
+            // 
+            this.labelNumberOfBags.AutoSize = true;
+            this.labelNumberOfBags.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumberOfBags.Location = new System.Drawing.Point(466, 138);
+            this.labelNumberOfBags.Name = "labelNumberOfBags";
+            this.labelNumberOfBags.Size = new System.Drawing.Size(59, 20);
+            this.labelNumberOfBags.TabIndex = 10;
+            this.labelNumberOfBags.Text = "BAGS";
+            // 
+            // labelCosts
+            // 
+            this.labelCosts.AutoSize = true;
+            this.labelCosts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCosts.Location = new System.Drawing.Point(466, 175);
+            this.labelCosts.Name = "labelCosts";
+            this.labelCosts.Size = new System.Drawing.Size(68, 20);
+            this.labelCosts.TabIndex = 11;
+            this.labelCosts.Text = "COSTS";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(581, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(581, 138);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(581, 175);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label1VolumeTotal
+            // 
+            this.label1VolumeTotal.AutoSize = true;
+            this.label1VolumeTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1VolumeTotal.Location = new System.Drawing.Point(31, 222);
+            this.label1VolumeTotal.Name = "label1VolumeTotal";
+            this.label1VolumeTotal.Size = new System.Drawing.Size(83, 24);
+            this.label1VolumeTotal.TabIndex = 15;
+            this.label1VolumeTotal.Text = "TOTAL ";
+            // 
+            // textBoxVolumeTotal
+            // 
+            this.textBoxVolumeTotal.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxVolumeTotal.Location = new System.Drawing.Point(235, 226);
+            this.textBoxVolumeTotal.Name = "textBoxVolumeTotal";
+            this.textBoxVolumeTotal.Size = new System.Drawing.Size(100, 20);
+            this.textBoxVolumeTotal.TabIndex = 16;
+            // 
+            // buttonCalculateChlorine
+            // 
+            this.buttonCalculateChlorine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonCalculateChlorine.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculateChlorine.Location = new System.Drawing.Point(37, 281);
+            this.buttonCalculateChlorine.Name = "buttonCalculateChlorine";
+            this.buttonCalculateChlorine.Size = new System.Drawing.Size(217, 38);
+            this.buttonCalculateChlorine.TabIndex = 17;
+            this.buttonCalculateChlorine.Text = "CALCULATE CHLORINE";
+            this.buttonCalculateChlorine.UseVisualStyleBackColor = false;
+            // 
+            // buttonClearAllFields
+            // 
+            this.buttonClearAllFields.BackColor = System.Drawing.Color.Yellow;
+            this.buttonClearAllFields.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClearAllFields.Location = new System.Drawing.Point(310, 281);
+            this.buttonClearAllFields.Name = "buttonClearAllFields";
+            this.buttonClearAllFields.Size = new System.Drawing.Size(214, 38);
+            this.buttonClearAllFields.TabIndex = 18;
+            this.buttonClearAllFields.Text = "CLEAR ";
+            this.buttonClearAllFields.UseVisualStyleBackColor = false;
+            // 
+            // buttonCloseApp
+            // 
+            this.buttonCloseApp.BackColor = System.Drawing.Color.Red;
+            this.buttonCloseApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCloseApp.Location = new System.Drawing.Point(572, 281);
+            this.buttonCloseApp.Name = "buttonCloseApp";
+            this.buttonCloseApp.Size = new System.Drawing.Size(216, 38);
+            this.buttonCloseApp.TabIndex = 19;
+            this.buttonCloseApp.Text = "EXIT";
+            this.buttonCloseApp.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 336);
+            this.Controls.Add(this.buttonCloseApp);
+            this.Controls.Add(this.buttonClearAllFields);
+            this.Controls.Add(this.buttonCalculateChlorine);
+            this.Controls.Add(this.textBoxVolumeTotal);
+            this.Controls.Add(this.label1VolumeTotal);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.labelCosts);
+            this.Controls.Add(this.labelNumberOfBags);
+            this.Controls.Add(this.labelKgsNeeded);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelDepthOfPool);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelWidthOfPool);
+            this.Controls.Add(this.textBoxLengthOfPool);
+            this.Controls.Add(this.labelLength);
+            this.Controls.Add(this.labelAppTitle);
+            this.Controls.Add(this.labelChlorineNeeded);
+            this.Controls.Add(this.labelVolumeOfWater);
+            this.Name = "Form1";
+            this.Text = "CHLORINE CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVolumeOfWater;
+        private System.Windows.Forms.Label labelChlorineNeeded;
+        private System.Windows.Forms.Label labelAppTitle;
+        private System.Windows.Forms.Label labelLength;
+        private System.Windows.Forms.TextBox textBoxLengthOfPool;
+        private System.Windows.Forms.Label labelWidthOfPool;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelDepthOfPool;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelKgsNeeded;
+        private System.Windows.Forms.Label labelNumberOfBags;
+        private System.Windows.Forms.Label labelCosts;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1VolumeTotal;
+        private System.Windows.Forms.TextBox textBoxVolumeTotal;
+        private System.Windows.Forms.Button buttonCalculateChlorine;
+        private System.Windows.Forms.Button buttonClearAllFields;
+        private System.Windows.Forms.Button buttonCloseApp;
+    }
+}
+
